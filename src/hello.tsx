7e01c99cd611a6bf311eb9b9927f9daa9d020eed
Freewind <nowindlee@gmail.com>
2023-12-13 21:20:49 +0800

    typescript-react-antd-tree-select-demo

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,30 @@
-import React from 'react'
-import {Alert} from 'antd';
+import React, {useState} from 'react'
+import {TreeSelect} from 'antd';
 
+type Option = {
+    label: string,
+    value: string,
+    children?: Option[]
+}
+const treeData: Option[] = [
+    {
+        label: 'a1', value: 'a1', children: [
+            {label: 'a2', value: 'a2'},
+            {label: 'a3', value: 'a3'},
+        ]
+    },
+    {
+        label: 'b1', value: 'b1', children: [
+            {label: 'b2', value: 'b2'},
+            {label: 'b3', value: 'b3'},
+        ]
+    }
+]
 export default function Hello() {
-  return <Alert message={'Hello'}/>
+    const [value, setValue] = useState<string>('b1')
+
+    return <div>
+        <TreeSelect style={{width: 200}} treeData={treeData} value={value} onChange={v => setValue(v)}/>
+        <div>{JSON.stringify(value)}</div>
+    </div>
 };
